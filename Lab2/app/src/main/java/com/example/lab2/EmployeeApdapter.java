114eb53276e7e34e724a6cf746bce6f5112de0bc
anhhuybee <ruakeu2002bee@gmail.com>
2023-04-11 20:52:43 +0700

    Lab2

---
+++ Lab2/app/src/main/java/com/example/lab2/EmployeeApdapter.java
@@ -0,0 +1,17 @@
+package com.example.lab2;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class EmployeeApdapter{
+
+}
