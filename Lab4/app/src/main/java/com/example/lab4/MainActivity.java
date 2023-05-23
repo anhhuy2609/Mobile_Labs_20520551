a61fe0c415f9dca5807f87d7fcec231355f0fdc9
anhhuybee <ruakeu2002bee@gmail.com>
2023-05-24 02:21:52 +0700

    Add file Lab4

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.java
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btn_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(switchActivityIntent);
+                finishAffinity();
+            }
+        });
     }
 }
